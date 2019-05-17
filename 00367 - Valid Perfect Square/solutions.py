f2ce248dcf75dabea3f2e5ee79b3317d8574e923
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-05-17 11:21:15 +0530

    Create solutions.py

---
+++ 00367 - Valid Perfect Square/solutions.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        
+        res = num ** .5
+        
+        return int(res) == res
