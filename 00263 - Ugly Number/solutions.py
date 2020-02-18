f331719aa9f13e9d148a493626d025e76081fbbb
Nitin Goyal <nitingoyal.dev@gmail.com>
2020-02-18 16:46:25 +0530

    Create solutions.py

---
+++ 00263 - Ugly Number/solutions.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        
+        if (num < 1):
+            return False
+        
+        while (num % 5) == 0:
+            num = num / 5
+
+        while (num % 3) == 0:
+            num = num / 3
+
+        while (num % 2) == 0:
+            num = num / 2
+                
+                
+        return num == 1
